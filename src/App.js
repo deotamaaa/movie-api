a919cea532e7fd34ec3b7d498cc955d52e1e1f00
Deotamaaa <deotama11@Gmail.com>
2022-11-30 15:52:45 +0700

    finish the web app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from './api'
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img className="movie-image" src={`${process.env.REACT_APP_BASEIMG}/${movie.poster_path}`} alt='' />
+          <div className="movie-date">release date : {movie.release_date}</div>
+          <div className="movie-rate">rating : {movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+      console.log({ query: query })
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          THE MOVIE DB API
+        </h1>
+        <input
+          type="text"
+          className='movie-search'
+          placeholder='What movies you want to search ?'
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
